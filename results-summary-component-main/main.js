803067875644b38db2f692c1437e6699499590d4
Domagoj Babic <domagoj.babic1303@gmail.com>
2025-02-24 12:06:31 +0100

    fixed JSON data not displaying

--- results-summary-component-main/main.js
+++ results-summary-component-main/main.js
@@ -3,15 +3,21 @@ console.log("Hello from JS");
 fetch("./data.json")
   .then((response) => response.json())
   .then((data) => {
-    const summaryItems = document.querySelectorAll("li div:first-child");
+    console.log(data);
+    const summaryArr = document.querySelectorAll("li div:first-child");
+    console.log(summaryArr);
+    const scoreArr = document.querySelectorAll(".score");
+    console.log(scoreArr);
 
     data.forEach((element, index) => {
-      const img = summaryItems[index].querySelector("img");
-      const title = summaryItems[index].querySelector("span");
+      const img = summaryArr[index].querySelector("img");
+      const title = summaryArr[index].querySelector("span");
+      const score = scoreArr[index].querySelector("span");
 
       img.src = element.icon;
       img.alt = element.category;
       title.textContent = element.category;
+      score.textContent = element.score;
     });
   })
   .catch((error) => {
