5ff02fdd462214f2a0e45549a143c20ed263f22c
Domagoj Babic <domagoj.babic1303@gmail.com>
2025-06-13 18:08:48 +0200

    fixed toggle, added svg, work on styles

--- browser-extensions-manager-ui-main/main.js
+++ browser-extensions-manager-ui-main/main.js
@@ -1,1 +1,32 @@
 console.log("Hello from JS");
+
+async function populateData() {
+  let data = await fetch("./data.json").then((response) => response.json());
+  let container = document.querySelector(".ext-container");
+
+  data.forEach((ele, i) => {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+            <div>
+              <img src="${ele.logo}" alt="${ele.name}" />
+              <div>
+                <h1>${ele.name}</h1>
+                <p>${ele.description}</p>
+              </div>
+            </div>
+            <div>
+              <button type="button" class="remove-button">Remove</button>
+              <label class="switch-label">
+                <input class="switch-input" type="checkbox" ${
+                  ele.isActive ? "checked" : ""
+                }/>
+                <span class="switch"></span>
+              </label>
+            </div>
+    `;
+    container.appendChild(card);
+  });
+}
+
+populateData();
